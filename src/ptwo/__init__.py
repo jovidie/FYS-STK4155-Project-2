ddd1bc44bee59f7c4e36c3d6dab9af87b3a57eed
Janita Willumsen <janitaws@icloud.com>
2024-10-14 19:05:54 +0200

    Add module init

---
+++ src/ptwo/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+import importlib.metadata
+
+# Example
+# from .models import NeuralNet
+
+__version__ = importlib.metadata.version(__package__)
+
+# __all__ = [
+#     "NeuralNet"
+# ]
