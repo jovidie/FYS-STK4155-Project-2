cd7819f46ad9ff251ccd648a1bc15054f9b45e6c
evengar <evengar@uio.no>
2024-10-21 08:14:05 +0200

    moved gradient functions to gradients.py

---
+++ src/ptwo/gradients.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def grad_OLS():
+    def grad_fun(X, y, theta):
+        n = y.shape[0]
+        return (2.0/n) * X.T @ (X @ theta - y)
+    return grad_fun
+
+def grad_ridge(lmb):
+    def grad_fun(X, y, theta):
+        n = y.shape[0]
+        return -(2.0/n) * X.T @ (X @ theta - y) + 2*lmb*theta
+    return(grad_fun)
--- src/ptwo/optimizers.py
+++ src/ptwo/optimizers.py
@@ -52,14 +52,3 @@ class RMSProp:
         update = learning_rate * grad / (self.delta+np.sqrt(self.Giter))
         return update
 
-def grad_OLS():
-    def grad_fun(X, y, theta):
-        n = y.shape[0]
-        return (2.0/n) * X.T @ (X @ theta - y)
-    return grad_fun
-
-def grad_ridge(lmb):
-    def grad_fun(X, y, theta):
-        n = y.shape[0]
-        return -(2.0/n) * X.T @ (X @ theta - y) + 2*lmb*theta
-    return(grad_fun)
