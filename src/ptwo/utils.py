c33da203d638cf2486d9acdeaecfcf03c0198413
evengar <evengar@uio.no>
2024-10-31 14:48:23 +0100

    Added franke function

--- src/ptwo/utils.py
+++ src/ptwo/utils.py
@@ -20,3 +20,10 @@ def calculate_polynomial(x, *args):
     for i in range(len(args)):
         y += args[i] * x**i
     return y
+
+def franke_function(x,y):
+    term1 = 0.75*np.exp(-(0.25*(9*x-2)**2) - 0.25*((9*y-2)**2))
+    term2 = 0.75*np.exp(-((9*x+1)**2)/49.0 - 0.1*(9*y+1))
+    term3 = 0.5*np.exp(-(9*x-7)**2/4.0 - 0.25*((9*y-3)**2))
+    term4 = -0.2*np.exp(-(9*x-4)**2 - (9*y-7)**2)
+    return term1 + term2 + term3 + term4
