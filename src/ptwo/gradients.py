f2a5b42b31f8306769b681d1200ac9de1033b871
evengar <evengar@uio.no>
2024-11-01 15:32:01 +0100

    fixed error in ridge gradient

--- src/ptwo/gradients.py
+++ src/ptwo/gradients.py
@@ -9,5 +9,5 @@ def grad_OLS():
 def grad_ridge(lmb):
     def grad_fun(X, y, theta):
         n = y.shape[0]
-        return -(2.0/n) * X.T @ (X @ theta - y) + 2*lmb*theta
+        return (2.0/n) * X.T @ (X @ theta - y) + 2*lmb*theta
     return(grad_fun)
