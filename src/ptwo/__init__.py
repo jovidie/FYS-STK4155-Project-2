794f79e6545770078be1eea9f22e5390a8c689cb
evengar <evengar@uio.no>
2024-11-04 10:36:34 +0100

    added utils for grid search with GD

--- src/ptwo/__init__.py
+++ src/ptwo/__init__.py
@@ -10,7 +10,7 @@ from .optimizers import ADAM, AdaGrad, RMSProp, lr_scheduler
 from .gradients import grad_OLS, grad_ridge
 from .activators import ReLU, leaky_ReLU, softmax, softmax_vec, sigmoid
 from .costfuns import mse, cross_entropy, binary_cross_entropy
-from .utils import calculate_polynomial, franke_function
+from .utils import calculate_polynomial, franke_function, GD_lambda_mse, eta_lambda_grid, lambda_lr_heatmap
 
 __version__ = importlib.metadata.version(__package__)
 
@@ -33,5 +33,8 @@ __all__ = [
     "cross_entropy",
     "binary_cross_entropy",
     "calculate_polynomial",
-    "franke_function"
+    "franke_function",
+    "GD_lambda_mse",
+    "",
+    ""
 ]
--- src/ptwo/utils.py
+++ src/ptwo/utils.py
@@ -27,3 +27,37 @@ def franke_function(x,y):
     term3 = 0.5*np.exp(-(9*x-7)**2/4.0 - 0.25*((9*y-3)**2))
     term4 = -0.2*np.exp(-(9*x-4)**2 - (9*y-7)**2)
     return term1 + term2 + term3 + term4
+
+# utilities for doing and plotting grid search with gradient descent
+
+def GD_lambda_mse(
+    X_train, X_test, y_train, y_test, learning_rate, lmbs, n_iter, batch_size=None, optimizer=None, gradient_fun=grad_ridge):
+    mses=np.zeros(len(lmbs))
+    for i in range(len(lmbs)):
+        gd = GradientDescent(learning_rate, gradient_fun(lmbs[i]), optimizer=optimizer)
+        gd.descend(X_train, y_train, n_iter, batch_size)
+        y_pred = X_test @ gd.theta
+        mses[i] = mse(y_test, y_pred)
+    return mses
+
+def eta_lambda_grid(
+    X_train, X_test, y_train, y_test, learning_rates, lmbs, n_iter, batch_size=None, optimizer=None, gradient_fun=grad_ridge
+    ):
+    mses = np.zeros( (len(lmbs), len(learning_rates)) )
+    for i in range(len(learning_rates)):
+        mse_eta = GD_lambda_mse(
+            X_train, X_test, y_train, y_test, learning_rate=learning_rates[i], lmbs=lmbs, n_iter=n_iter, batch_size=batch_size, optimizer=optimizer, gradient_fun=gradient_fun
+            )
+        mses[:,i] = mse_eta
+    return mses
+
+def lambda_lr_heatmap(mses, lmbs, learning_rates, lmb_label_res=3, lr_label_res=3):
+    lmb_lab = ["{0:.2e}".format(x) for x in lmbs]
+    lr_lab = ["{0:.2e}".format(x) for x in learning_rates]
+
+    sns.heatmap(mses, annot=True)
+    plt.xticks(np.arange(len(learning_rates))[1::lmb_label_res] + 0.5, lr_lab[1::lmb_label_res])
+    plt.yticks(np.arange(len(lmbs))[1::lr_label_res] + 0.5, lmb_lab[1::lr_label_res])
+    plt.xlabel(r"Learning rate $\eta$")
+    plt.ylabel(r"$\lambda$")
+    plt.show()
