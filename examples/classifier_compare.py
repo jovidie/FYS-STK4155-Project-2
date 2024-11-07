59a16fca217a6828ab0af89d0785c1206839bde6
ellenbet-uio <ellenbet@ulrik.uio.no>
2024-11-07 11:16:41 +0100

    backpropagation and loss functions, as well as more on activation functions

--- examples/classifier_compare.py
+++ examples/classifier_compare.py
@@ -7,9 +7,14 @@ from ptwo.models import NeuralNetwork
 from ptwo.activators import sigmoid, relu6
 from ptwo.costfuns import binary_cross_entropy
 from ptwo.optimizers import ADAM
-from sklearn.metrics import ConfusionMatrixDisplay, confusion_matrix
+from sklearn.metrics import ConfusionMatrixDisplay, confusion_matrix, accuracy_score
+import matplotlib.pyplot as plt
+from ptwo.plot import set_plt_params
 
-#importing data:
+# standard settings for plots
+set_plt_params()
+
+# importing data:
 data = pd.read_csv("./data/wisconsin_breast_cancer_data.csv")
 print(f"DATA\n{data}")
 
@@ -22,27 +27,29 @@ total_Ms = np.sum(targets)
 network_input = network_input.to_numpy()
 targets = targets.to_numpy()
 
+# checking if our data set is balanced
 print(f"\n ---- {total_Ms} malignant tumors \n ---- {int(len(targets) - total_Ms)} benign tumors\n")
 
+# making targets into two columns for each option (binary) "B" and "M"
 targets_percent = np.zeros((targets.shape[0], 2))
 for i, t in enumerate(targets):
     targets_percent[i, t] = 1
 
+# setting seed for reproducibility
+np.random.seed(60)
+
 # splitting and scaling: 
 train_in, test_in, train_o, test_o = train_test_split(network_input, targets_percent, test_size = 0.2)
 standard_scaler = StandardScaler()
-#minmax_scaler = MinMaxScaler()
 train_in = standard_scaler.fit_transform(train_in)
 test_in = standard_scaler.transform(test_in)
-#train_in = minmax_scaler.fit_transform(train_in)
-#test_in = minmax_scaler.transform(test_in)
 
 #joint parameters
-np.random.seed(42)
-epochs = 500
+epochs = 200
 lrate = 0.1
-layer_output_sizes = [100,  2]
-activators = [relu6, sigmoid]
+layer_output_sizes = [100, 2]
+activators = [sigmoid, sigmoid]
+data_title = "ADAM_sigmoid-100_sigmoid-2"
 
 print(f"\n\n ------------ Building neural networks with {len(layer_output_sizes)} layer for Sklearn and own FFNN ------------ \n")
 #sklearn classifier: 
@@ -56,12 +63,23 @@ print(f"\n\n ------------ Training network with {epochs} epochs and {round(lrate
 NN = NeuralNetwork(network_input_size = network_input.shape[1], layer_output_sizes = layer_output_sizes, activation_funcs = activators, optimizer = ADAM(), cost_function = binary_cross_entropy, classification = True)
 NN.predict(train_in)
 #print(NN.predictions)
-NN.train_network(train_in, train_o, epochs = epochs, learning_rate = lrate, verbose = True, batch_size = 150)
+NN.kfold_train(train_in, train_o, epochs = epochs, learning_rate = lrate, verbose = True, batch_size = 150, k = 10)
 test_pred = NN.predict(test_in)
 
-print("SKLEARN NN", clf.score(test_in, test_o))
-print("OUR NN", NN.accuracy(test_in, test_o))
+# making predictions back into vector of 0 and 1's 
+predi = np.argmax(test_pred, axis=1)
+clf_predictions = np.argmax(clf_test_pred, axis=1)
+golden = np.argmax(test_o, axis=1)
 
-# confusion matrix - this doesnt work...
-#conf = confusion_matrix(test_o, clf_test_pred)
-#ConfusionMatrixDisplay(test_o, clf_test_pred).plot(conf)
+#confusion matrices
+conf1 = confusion_matrix(golden, clf_predictions, labels = [0, 1])
+ConfusionMatrixDisplay(conf1).plot()
+plt.title(f"Confusion matrix, Sklearn's NN, acc: {round(accuracy_score(clf_predictions, golden), 4)}")
+plt.savefig("./latex/figures/sklearnWBC_final_" + data_title + ".pdf", bbox_inches = "tight")
+plt.show()
+
+conf2 = confusion_matrix(golden, predi, labels = [0, 1])
+ConfusionMatrixDisplay(conf2).plot()
+plt.title(f"Confusion matrix, our NN, acc: {round(accuracy_score(predi, golden), 4)}")
+plt.savefig("./latex/figures/ourWBC_final_"+ data_title + ".pdf", bbox_inches = "tight")
+plt.show()
