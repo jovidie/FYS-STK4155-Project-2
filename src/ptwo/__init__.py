867e17c74702ae0d5f919320a850ac85dcde59e0
Janita Willumsen <janitaws@icloud.com>
2024-11-08 17:49:54 +0100

    Add function to plot confusion

--- src/ptwo/__init__.py
+++ src/ptwo/__init__.py
@@ -11,7 +11,7 @@ from .gradients import grad_OLS, grad_ridge
 from .activators import ReLU, leaky_ReLU, softmax, softmax_vec, sigmoid
 from .costfuns import mse, cross_entropy, binary_cross_entropy
 from .utils import calculate_polynomial, franke_function, GD_lambda_mse, eta_lambda_grid, lambda_lr_heatmap, preprocess_cancer_data
-from .plot import set_plt_params, plot_heatmap, plot_mse, plot_mse_r2
+from .plot import set_plt_params, plot_heatmap, plot_mse, plot_mse_r2, plot_loss_acc, plot_confusion
 
 __version__ = importlib.metadata.version(__package__)
 
@@ -43,5 +43,7 @@ __all__ = [
     "set_plt_params",
     "plot_heatmap",
     "plot_mse",
-    "plot_mse_r2"
+    "plot_mse_r2", 
+    "plot_loss_acc",
+    "plot_confusion"
 ]
